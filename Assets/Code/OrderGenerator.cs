78806343cb554296fcbcfff89a8ed2c2dd9116f6
AugustoChies <guto_chies@hotmail.com>
2018-11-16 00:31:04 -0200

    finished order generator

--- Assets/Code/OrderGenerator.cs
+++ Assets/Code/OrderGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum Dificulty
 { easy,medium,hard};
@@ -14,8 +15,25 @@ public class OrderGenerator : MonoBehaviour {
     public Preparation[] possiblePreparations;
     public CookPoint[] possibleCooking;
     public Dificulty dificulty;
-	// Use this for initialization
-	void Start () {
+
+    public IngredientIconMap iic;
+    public IngredientPrepMap pic;
+    public IngredientCookMap cic;
+    protected GameObject ing1, ing2, ing3, ing4, ing5, ing6, ing7, ing8, ing9, fillbar;
+    public Transform iconmaster;
+    // Use this for initialization
+    void Start () {
+        ing1 = iconmaster.Find("Ing1").gameObject;
+        ing2 = iconmaster.Find("Ing2").gameObject;
+        ing3 = iconmaster.Find("Ing3").gameObject;
+        ing4 = iconmaster.Find("Ing4").gameObject;
+        ing5 = iconmaster.Find("Ing5").gameObject;
+        ing6 = iconmaster.Find("Ing6").gameObject;
+        ing7 = iconmaster.Find("Ing7").gameObject;
+        ing8 = iconmaster.Find("Ing8").gameObject;
+        ing9 = iconmaster.Find("Ing9").gameObject;
+        fillbar = iconmaster.Find("FillBar").gameObject;
+
         Generate();
 	}
 	
@@ -27,6 +45,8 @@ public class OrderGenerator : MonoBehaviour {
             Generate();
             remainingTime = time;              
         }
+
+        fillbar.GetComponent<Image>().fillAmount = remainingTime / time;
 	}
 
     public void Generate()
@@ -65,14 +85,14 @@ public class OrderGenerator : MonoBehaviour {
                 ingredientcount = Random.Range(1, 4);
 
                 randn = Random.Range(0, possibleCooking.Length);
-                CookPoint cp;
-                if (randn < 3)
+                
+                if (randn < 2)
                 {
                     cp = CookPoint.raw;
                 }
                 else
                 {
-                    cp = possibleCooking[randn - 2];
+                    cp = possibleCooking[randn - 1];
                 }
 
                 for (int i = 0; i < ingredientcount; i++)
@@ -87,11 +107,94 @@ public class OrderGenerator : MonoBehaviour {
                 }
                 break;
             case Dificulty.hard:
+                ingredientcount = Random.Range(2, 4);                
+
+                for (int i = 0; i < ingredientcount; i++)
+                {
+                    randn = Random.Range(0, possibleingredients.Length);
+                    IngredientType tp = possibleingredients[randn];
+                    randn = Random.Range(0, possiblePreparations.Length);
+                    Preparation pr = possiblePreparations[randn];
+                    randn = Random.Range(0, possibleCooking.Length);
+                    cp = possibleCooking[randn];
+
+                    Ingredient ing = new Ingredient(tp, pr, cp);
+                    ingredientsgen.Add(ing);
+                }
                 break;
             default:
                 break;
         }
 
+        if(myOrder != null)
+            ShowIcons(false, myOrder);
         myOrder = new Food(ingredientsgen);
+        ShowIcons(true,myOrder);
+    }
+
+    public void ShowIcons(bool yes, Food placed)
+    {
+        fillbar.SetActive(yes);
+        if (ing1 != null && placed.ingredients.Count > 0)
+        {
+            ing1.GetComponent<Image>().sprite = iic.pairs[placed.ingredients[0].type];
+            ing1.SetActive(yes);
+            if (placed.ingredients[0].IsPrepared())
+            {
+                ing4.GetComponent<Image>().sprite = pic.pairs[placed.ingredients[0].preparation];
+                ing4.SetActive(yes);
+            }
+            else
+                ing4.SetActive(false);
+
+            if (placed.ingredients[0].IsCooked())
+            {
+                ing7.GetComponent<Image>().sprite = cic.pairs[placed.ingredients[0].point];
+                ing7.SetActive(yes);
+            }
+            else
+                ing7.SetActive(false);
+        }
+        if (ing2 != null && placed.ingredients.Count > 1)
+        {
+            ing2.GetComponent<Image>().sprite = iic.pairs[placed.ingredients[1].type];
+            ing2.SetActive(yes);
+            if (placed.ingredients[1].IsPrepared())
+            {
+                ing5.GetComponent<Image>().sprite = pic.pairs[placed.ingredients[1].preparation];
+                ing5.SetActive(yes);
+            }
+            else
+                ing5.SetActive(false);
+
+            if (placed.ingredients[1].IsCooked())
+            {
+                ing8.GetComponent<Image>().sprite = cic.pairs[placed.ingredients[1].point];
+                ing8.SetActive(yes);
+            }
+            else
+                ing8.SetActive(false);
+        }
+        if (ing3 != null && placed.ingredients.Count > 2)
+        {
+            ing3.GetComponent<Image>().sprite = iic.pairs[placed.ingredients[2].type];
+            ing3.SetActive(yes);
+
+            if (placed.ingredients[2].IsPrepared())
+            {
+                ing6.GetComponent<Image>().sprite = pic.pairs[placed.ingredients[2].preparation];
+                ing6.SetActive(yes);
+            }
+            else
+                ing6.SetActive(false);
+
+            if (placed.ingredients[2].IsCooked())
+            {
+                ing9.GetComponent<Image>().sprite = cic.pairs[placed.ingredients[2].point];
+                ing9.SetActive(yes);
+            }
+            else
+                ing9.SetActive(false);
+        }
     }
 }
