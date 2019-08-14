014be7d382bb43343a8da8a70e8730ebd26fefe4
AugustoJC <guto_chies@hotmail.com>
2019-08-14 16:21:15 -0300

    started minigames screen

---
+++ Assets/Code/MinigamesScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigamesScript : MonoBehaviour {
+    [Header("Minigame time values")]
+    public float cutTime;
+    public float mashTime;
+    public float grateTime;
+
+    [Header("Minigame time reduction values")]
+    public float cutRedTime;
+    public float mashRedTime;
+    public float grateRedTime;
+
+    [Header("References")]
+    public GameObject cutMinigame;
+    public GameObject mashMinigame;
+    public GameObject grateMinigame;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
