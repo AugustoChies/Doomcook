9ae97edbb4c8a9d7d78122bf5ff07e7f69acfe4d
AugustoChies <guto_chies@hotmail.com>
2019-08-15 10:40:54 -0300

    minigame almost done

--- Assets/Code/MinigamesScript.cs
+++ Assets/Code/MinigamesScript.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MinigamesScript : MonoBehaviour {
+    public bool minigameOn;
+    public KeycodesReference inputMap;
     [Header("Minigame time values")]
     public float cutTime;
     public float mashTime;
@@ -18,14 +20,42 @@ public class MinigamesScript : MonoBehaviour {
     public GameObject mashMinigame;
     public GameObject grateMinigame;
 
-
+    [Header("Cut game variables")]
+    public float barSpeed;
+    public float middleTolerance;
+    Transform beginning;
+    Transform end;
+    GameObject bar;
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start() {
+        beginning = cutMinigame.transform.Find("Beginning");
+        end = cutMinigame.transform.Find("Ending");
+        bar = cutMinigame.transform.Find("Bar").gameObject;
+    }
+
+    public void StartCutting(float timeModifier)
+    {
+        StartCoroutine(CutGame(timeModifier));
+    }
+
+    IEnumerator CutGame(float modifier)
+    {
+        minigameOn = true;
+        bool right = true;
+        float elapsedTime = cutTime + modifier;
+        float barPosition = 0;
+        bar.transform.position = beginning.position;
+        while (elapsedTime > 0)
+        {
+            if(Input.GetKeyDown(inputMap.interact))
+            {
+
+            }
+            bar.transform.position = Vector3.Lerp(beginning.position,end.position,barPosition);
+            elapsedTime -= Time.deltaTime;
+
+
+            yield return null;
+        }
+    }
 }
