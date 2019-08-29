fa89c364459956cff7532ee5e638872490e8d2e3
AugustoChies <guto_chies@hotmail.com>
2019-08-29 10:27:45 -0300

    upgrade scriptables

---
+++ Assets/Code/Variables/UpgradeRequirements.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UpgradeIDs
+{
+    strongwall,strongestwall,autoprep,miniprep,fastburn,noburn,
+    table,snack,screen
+};
+
+[CreateAssetMenu]
+public class UpgradeRequirements : ScriptableObject
+{
+    public List<UpgradeIDs> iDs;
+    public List<int> stars;
+    public List<int> money;
+
+    public void CheckAvailability()
+    {
+        //stuff
+    }
+}
---
+++ Assets/Code/Variables/UpgradesStatus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WallUpgrade
+{
+    standart,great,greater
+};
+
+public enum PrepUpgrade
+{
+    standart,minigame,auto
+};
+
+public enum OvenUpgrade
+{
+    standart,fast,unburning
+};
+
+
+[CreateAssetMenu]
+public class UpgradesStatus : ScriptableObject
+{
+    public WallUpgrade wallUpgrade;
+    public PrepUpgrade prepUpgrade;
+    public OvenUpgrade ovenUpgrade;
+    public float tableCount;
+    public float snackCount;
+    public List<bool> screens;
+
+    public int money, stars;
+}
