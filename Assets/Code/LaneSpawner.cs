43451d9b5401d10a522b80287a8177a9737ef5d5
AugustoJC <guto_chies@hotmail.com>
2019-09-03 16:16:45 -0300

    continued enemy spawner

--- Assets/Code/LaneSpawner.cs
+++ Assets/Code/LaneSpawner.cs
@@ -2,8 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MonsterType
+{
+    zomboid,mole,blaze,flayer
+};
+
+public struct MonsterData
+{
+    MonsterType type;
+    int foodIndex;
+    float spawnTime;
+};
 public class LaneSpawner : MonoBehaviour
 {
+    public List<MonsterData> monsters;
+    public GameState gs;
+
     // Start is called before the first frame update
     void Start()
     {
