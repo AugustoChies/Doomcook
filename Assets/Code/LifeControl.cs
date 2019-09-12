e03359ebf020d1cf51ec3c0a4e187924fce8ad72
AugustoChies <guto_chies@hotmail.com>
2019-09-12 01:21:26 -0300

    wall upgrades

--- Assets/Code/LifeControl.cs
+++ Assets/Code/LifeControl.cs
@@ -7,13 +7,37 @@ using UnityEngine.UI;
 public class LifeControl : MonoBehaviour {
     public FloatVariable life;
     public Image lifebar;
+    public float maxLife = 100;
+    public UpgradesStatus upgrades;
+    public WallUpgrade myupgrade;
+    public GameState gs;
+    public float normalLife, life1, life2;
 	// Use this for initialization
 	void Start () {
-        life.Value = 100;
+        life.Value = maxLife;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        lifebar.fillAmount = life.Value / 100;
+        if (gs.upgrading)
+        {
+            myupgrade = upgrades.wallUpgrade;
+            switch (myupgrade)
+            {
+                case WallUpgrade.standart:
+                    maxLife = normalLife;
+                    break;
+                case WallUpgrade.great:
+                    maxLife = life1;
+                    break;
+                case WallUpgrade.greater:
+                    maxLife = life2;
+                    break;
+                default:
+                    break;
+            }
+            life.Value = maxLife;
+        }
+        lifebar.fillAmount = life.Value / maxLife;
 	}
 }
