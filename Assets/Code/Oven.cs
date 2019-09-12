ca2b841626ba0d33f8d00e84db28b44f97041e01
AugustoJC <guto_chies@hotmail.com>
2019-09-12 16:09:45 -0300

    oven upgrades

--- Assets/Code/Oven.cs
+++ Assets/Code/Oven.cs
@@ -6,6 +6,10 @@ public class Oven : KitchenCell{
     public float timer;
     public FloatReference prepTime;
     public GameObject meter,pointer;
+    public GameState gs;
+    public UpgradesStatus upgrades;
+    public OvenUpgrade myupgrade;
+    public float quickmultiplier, slowmultiplier;
 
     // Use this for initialization
     void Start () {
@@ -15,10 +19,30 @@ public class Oven : KitchenCell{
 	
 	// Update is called once per frame
 	void Update () {
+        if(gs.upgrading)
+        {
+            myupgrade = upgrades.ovenUpgrade;
+        }
         if (preparing)
         {
-            timer += Time.deltaTime;
-            pointer.transform.localEulerAngles = Vector3.Lerp(new Vector3(60, 0, 89.9f), new Vector3(60, 0, -89.9f),timer/prepTime);
+            switch (myupgrade)
+            {
+                case OvenUpgrade.standart:
+                    timer += Time.deltaTime;
+                    pointer.transform.localEulerAngles = Vector3.Lerp(new Vector3(60, 0, 89.9f), new Vector3(60, 0, -89.9f), timer / prepTime);
+                    break;
+                case OvenUpgrade.fast:
+                    timer += Time.deltaTime;
+                    pointer.transform.localEulerAngles = Vector3.Lerp(new Vector3(60, 0, 89.9f), new Vector3(60, 0, -89.9f), timer / prepTime * quickmultiplier);
+                    break;
+                case OvenUpgrade.unburning:
+                    timer += Time.deltaTime;
+                    pointer.transform.localEulerAngles = Vector3.Lerp(new Vector3(60, 0, 89.9f), new Vector3(60, 0, -89.9f), timer / prepTime * slowmultiplier);
+                    break;
+                default:
+                    break;
+            }
+            
         }
     }
 
@@ -30,14 +54,42 @@ public class Oven : KitchenCell{
 
     public override Food TakeFood()
     {
-        for(int i = 0; i < placed.ingredients.Count; i++)
+        switch (myupgrade)
         {
-            placed.ingredients[i].point = (CookPoint)((timer/prepTime) * 4);
-            if(timer/prepTime * 4 > 4)
-            {
-                placed.ingredients[i].point = CookPoint.burned;
-            }
+            case OvenUpgrade.standart:
+                for (int i = 0; i < placed.ingredients.Count; i++)
+                {
+                    placed.ingredients[i].point = (CookPoint)((timer / prepTime) * 4);
+                    if (timer / prepTime * 4 > 4)
+                    {
+                        placed.ingredients[i].point = CookPoint.burned;
+                    }
+                }
+                break;
+            case OvenUpgrade.fast:
+                for (int i = 0; i < placed.ingredients.Count; i++)
+                {
+                    placed.ingredients[i].point = (CookPoint)((timer / prepTime * quickmultiplier) * 4);
+                    if (timer / prepTime * quickmultiplier * 4 > 4)
+                    {
+                        placed.ingredients[i].point = CookPoint.burned;
+                    }
+                }
+                break;
+            case OvenUpgrade.unburning:
+                for (int i = 0; i < placed.ingredients.Count; i++)
+                {
+                    placed.ingredients[i].point = (CookPoint)((timer / prepTime * slowmultiplier) * 4);
+                    if (timer / prepTime * slowmultiplier * 4 > 4)
+                    {
+                        placed.ingredients[i].point = CookPoint.welldone;
+                    }
+                }
+                break;
+            default:
+                break;
         }
+       
         
         preparing = false;
         timer = 0;
