0954e3a8a3f2d54d00c7f210ffce518274dcf0c9
AugustoChies <guto_chies@hotmail.com>
2019-12-02 02:42:15 -0300

    all stages and wall model

---
+++ Assets/Code/Kitchen Objects/WallModelController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallModelController : MonoBehaviour
+{
+    public GameState gs;
+    public UpgradesStatus upgrades;
+    public GameObject wall, strong, ultra;
+
+    private void Update()
+    {
+        if(gs.upgrading)
+        {
+            wall.SetActive(false);
+            strong.SetActive(false);
+            ultra.SetActive(false);
+            switch (upgrades.wallUpgrade)
+            {
+                case WallUpgrade.standart:
+                    wall.SetActive(true);
+                    break;
+                case WallUpgrade.great:
+                    strong.SetActive(true);
+                    break;
+                case WallUpgrade.greater:
+                    ultra.SetActive(true);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
